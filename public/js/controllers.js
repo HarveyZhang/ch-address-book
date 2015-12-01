8dfae42be6736408c512a1c1801f5d0e012137d1
Haowei Zhang <haoweiz@outlook.com>
2015-12-01 02:23:41 -0800

    add function to edit contact

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,15 +8,34 @@ addressBookControllers
         var DEFAULT_TITLE = 'CH Address Book';
 
         $rootScope.title = DEFAULT_TITLE;
-        $scope.loading = true;
+        $scope.state = {
+            loading: true,
+            editing: false
+        };
+
         addressBookService.query().then(function(contacts) {
             $scope.contacts = contacts;
-            $scope.loading = false;
-        })
+            $scope.state.loading = false;
+        });
 
         $scope.entrySelected = function(userId) {
-            $scope.selected = $scope.contacts[userId];
+            $scope.selected = angular.extend({}, $scope.contacts[userId]);
             $rootScope.title = DEFAULT_TITLE + ' - ' +
                 $scope.selected.firstname + ' ' + $scope.selected.lastname;
+        };
+
+        $scope.submit = function(form, newContact) {
+            if (form.$valid) {
+                angular.extend($scope.contacts[newContact.id], newContact);
+                $scope.state.editing = false;
+            }
+        };
+
+        $scope.reset = function(form, userId) {
+            if (form) {
+              form.$setPristine();
+              form.$setUntouched();
+            }
+            $scope.selected = angular.extend({}, $scope.contacts[userId]);
         }
     }]);
